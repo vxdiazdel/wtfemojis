55617f25944c5a91fef764744f9470a174d32b5c
Victor Diaz de Leon <vrdiazdel@gmail.com>
2017-02-21 20:38:18 -0800

    Update app to include copy to clipboard and add success or error notification

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,9 @@ const app = new Vue({
 	data: {
 		emojis: [],
 		searchText: '',
-		api: 'https://wtfemojisapi.herokuapp.com/wtf'
+		api: 'https://wtfemojisapi.herokuapp.com/wtf',
+		copied: false,
+		error: false
 	},
 	created: function() {
 		this.fetchEmojis()
@@ -23,13 +25,39 @@ const app = new Vue({
 		}
 	},
 	methods: {
-		fetchEmojis: () => {
+		fetchEmojis: function() {
 			return $.ajax({
 				method: 'GET',
 				url: 'https://wtfemojisapi.herokuapp.com/wtf',
 				crossDomain: true,
 				dataType: 'json'
 			});
+		},
+		copy: function(e) {
+			const copyText = e.target;
+			const range = document.createRange();
+
+			range.selectNode(copyText);
+			window.getSelection().addRange(range);
+
+			if (document.execCommand('copy')) {
+				this.copySuccess();
+			} else {
+				this.copyFail();
+			}
+		},
+		copySuccess: function() {
+			this.copied = true;
+			window.getSelection().removeAllRanges();
+			setTimeout(() => {
+				this.copied = false;
+			}, 1250);
+		},
+		copyFail: function() {
+			this.error = true;
+			setTimeout(() => {
+				this.error = false;
+			}, 1250);
 		}
 	}
 });
